a193df785a0478fa0cc9c3e8803a3ae101400760
jahangir Ahmed <jahangir4csit@gmail.com>
2022-01-07 15:20:31 +0600

    google font init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Creative IT Institute Bangladesh`,
+    description: `Creative IT Institute Website`,
+    author: `@Jahangir Ahmed`,
+    siteUrl: `https://creativeitinstitute.com/`,
   },
   plugins: [
     {
@@ -30,12 +30,18 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, 
       },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Hind+Siliguri:300,400,500,600,700`, `source sans pro:300,400,500,600,700`
+        ],
+        display: 'swap'
+      }
     },
     `gatsby-plugin-smoothscroll`
     // this (optional) plugin enables Progressive Web App + Offline functionality

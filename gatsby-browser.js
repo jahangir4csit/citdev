363a3299ac912060fe4082d87092c7029868ce0e
jahangir Ahmed <jahangir4csit@gmail.com>
2022-01-07 20:54:22 +0600

    window fix 2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,5 +11,3 @@ import "./node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './node_modules/slick-carousel/slick/slick.css'
 import './node_modules/slick-carousel/slick/slick-theme.css'
 import './node_modules/react-modal-video/css/modal-video.min.css';
-
-import './src/assets/js/script'

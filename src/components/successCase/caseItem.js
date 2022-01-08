3c12d37a6af69baaf7ebb210c670ca75beb45c9f
jahangir Ahmed <jahangir4csit@gmail.com>
2022-01-08 12:52:31 +0600

    video modal add

--- src/components/successCase/caseItem.js
+++ src/components/successCase/caseItem.js
@@ -1,16 +1,14 @@
 import React from "react";
+import { ModalVid } from "../utils/modal"; 
 
 export default function CaseItem(data){
     const caseData = data.data;
-    console.log(caseData, 'case data')
 
     return(        
         <div class="col-md-6">
             <div class="success_item">
                 {caseData.featuredImage && caseData.successStoryLink ?
-                <a class="venobox" data-autoplay="true" data-vbtype="video" href="https://youtu.be/eBnMWLMGq04">
-                    <img class="img-fluid w-100" src={caseData.featuredImage.node.sourceUrl} alt="image" />
-                </a>
+                <ModalVid videoData={caseData} />
                 : 'Blank Item'}
             </div>
         </div>
---
+++ src/components/utils/modal.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+import ReactDOM from 'react-dom'
+import ModalVideo from 'react-modal-video'
+
+export const ModalVid = (data) => {
+
+    const caseSingle = data.videoData
+    console.log(caseSingle, 'case single');
+    const [isOpen, setOpen] = useState(false)
+
+  return (
+    <React.Fragment>
+      <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId={caseSingle.successStoryLink.successStoryLink} onClose={() => setOpen(false)} />
+      <button className="modalvidwrap" onClick={()=> setOpen(true)}>
+        <img class="img-fluid w-100" src={caseSingle.featuredImage.node.sourceUrl} alt={caseSingle.title} />
+      </button>
+    </React.Fragment>
+  )
+}

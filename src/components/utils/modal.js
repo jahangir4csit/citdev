f226ace8ad3142f8c7b3d011f8d42a84c020dcd3
jahangir Ahmed <jahangir4csit@gmail.com>
2022-01-08 16:40:46 +0600

    window defined

--- src/components/utils/modal.js
+++ src/components/utils/modal.js
@@ -4,21 +4,22 @@ import ModalVideo from 'react-modal-video'
 
 
 export const ModalVid = (data) => {
-    if (typeof window === 'undefined') {
-        global.window = {}
-    }
+
     const caseSingle = data.videoData
     console.log(caseSingle, 'case single');
 
     const [isOpen, setOpen] = useState(false)
-
+    if (typeof window !== 'undefined' && typeof document !== 'undefined'){
+        console.log(window, 'window');
   return (
     <React.Fragment>
+    
       <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId={caseSingle.successStoryLink.successStoryLink} onClose={() => setOpen(false)} />
       <button className="modalvidwrap" onClick={()=> setOpen(true)}>
         <img class="img-fluid w-100" src={caseSingle.featuredImage.node.sourceUrl} alt={caseSingle.title} />
       </button>
     </React.Fragment>
   )
+    }
 
 }

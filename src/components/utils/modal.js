840b14ad3563c3de842024f340610a23d0730899
jahangir Ahmed <jahangir4csit@gmail.com>
2022-01-08 15:11:27 +0600

    window define

--- src/components/utils/modal.js
+++ src/components/utils/modal.js
@@ -1,27 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ReactDOM from 'react-dom'
 import ModalVideo from 'react-modal-video'
 
-class App extends React.Component {
 
-  constructor () {
-    super()
-    this.state = {
-      isOpen: false
-    }
-    this.openModal = this.openModal.bind(this)
-  }
+export const ModalVid = (data) => {
+    let window = window || global.window
+    const caseSingle = data.videoData
+    console.log(caseSingle, 'case single');
+    const [isOpen, setOpen] = useState(false)
 
-  openModal () {
-    this.setState({isOpen: true})
-  }
+  return (
+    <React.Fragment>
+      <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId={caseSingle.successStoryLink.successStoryLink} onClose={() => setOpen(false)} />
+      <button className="modalvidwrap" onClick={()=> setOpen(true)}>
+        <img class="img-fluid w-100" src={caseSingle.featuredImage.node.sourceUrl} alt={caseSingle.title} />
+      </button>
+    </React.Fragment>
+  )
 
-  render () {
-    return (
-      <React.Fragment>
-        <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId='L61p2uyiMSo' onClose={() => this.setState({isOpen: false})} />
-        <button onClick={this.openModal}>Open</button>
-      </React.Fragment>
-    )
-  }
 }
+
+ReactDOM.render(
+    <ModalVid />,
+      document.getElementById('root')
+  )

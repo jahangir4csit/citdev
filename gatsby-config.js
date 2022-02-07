31f29862d287b04629e3dc697d41fc474b8d14f3
jahangir Ahmed <jahangir4csit@gmail.com>
2022-02-07 09:08:39 +0600

    prdcuction time increase

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,20 @@ module.exports = {
       options: {
         url: `https://cit.creativejuniors.com/graphql`
         //url: `http://localhost/citbd/graphql`
-      }
+      },
+      develop: {
+        hardCacheMediaFiles: true,
+      },
+      type: {
+        Post: {
+          limit:
+            process.env.NODE_ENV === `development`
+              ? // Lets just pull 50 posts in development to make it easy on ourselves (aka. faster).
+                50
+              : // and we don't actually need more than 5000 in production for this particular site
+                5000,
+        },
+      },
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,

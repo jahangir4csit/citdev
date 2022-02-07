97f130b46d1604e7cf8afe6cbabd98555f578efc
jahangir Ahmed <jahangir4csit@gmail.com>
2022-02-07 10:24:26 +0600

    error handle 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,30 +12,6 @@ module.exports = {
         url: `https://cit.creativejuniors.com/graphql`
         //url: `http://localhost/citbd/graphql`
       },
-      schema: {
-        timeout: 50000,
-        perPage: 300,
-        requestConcurrency: 50,
-        previewRequestConcurrency: 50,
-      },
-      develop: {
-        hardCacheMediaFiles: true,
-      },
-      type: {
-        Post: {
-          limit:
-            process.env.NODE_ENV === `development`
-              ? // Lets just pull 50 posts in development to make it easy on ourselves (aka. faster).
-                50
-              : // and we don't actually need more than 5000 in production for this particular site
-                5000,
-        },
-      },
-      debug: {
-        graphql: {
-          showQueryVarsOnError: true,
-        },
-      },
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
@@ -56,15 +32,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
-        // Set to false to allow builds to continue on image errors
-        failOnError: true,
-        // deprecated options and their defaults:
-        base64Width: 20,
-        forceBase64Format: ``, // valid formats: png,jpg,webp
-        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
-        stripMetadata: true,
-        defaultQuality: 90, 
+        icon: `src/images/gatsby-icon.png`, 
       },
     },
     {

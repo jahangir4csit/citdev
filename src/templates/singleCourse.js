fc5c822d4cb0939e9aea9744589437623025e34d
jahangir Ahmed <jahangir4csit@gmail.com>
2022-02-10 00:06:56 +0600

    single empty 2

---
+++ src/templates/singleCourse.js
@@ -0,0 +1,26 @@
+import React,{Fragment, useState} from 'react'
+import Slider from "react-slick";
+import { graphql } from "gatsby"; 
+import { StaticImage } from "gatsby-plugin-image"
+import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Layout from "../components/layout"
+import StudentFeedback from "../components/homepage/studentFeedback"
+import SuccessStorySlider from '../components/successCase/SuccessStorySlider'
+import {faPlay} from '@fortawesome/free-solid-svg-icons'
+import ModalVideo from 'react-modal-video'
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
+import TabContainer from 'react-bootstrap/TabContainer'
+import TabContent from 'react-bootstrap/TabContent'
+import TabPane from 'react-bootstrap/TabPane'
+import Nav from 'react-bootstrap/Nav'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+export default function SingleCourse({data}){
+ return 'Single';   
+}
+
+
+

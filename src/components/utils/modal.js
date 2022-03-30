f9e3c50ec54072f020dded728a97add0e4345df4
jahangir Ahmed <jahangir4csit@gmail.com>
2022-03-30 17:55:23 +0600

    success story update

--- src/components/utils/modal.js
+++ src/components/utils/modal.js
@@ -16,8 +16,11 @@ export const ModalVid = (data) => {
     <React.Fragment>
       <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId={caseSingle.successStoryLink.successStoryLink} onClose={() => setOpen(false)} />
       <button className="modalvidwrap" onClick={()=> setOpen(true)}>
-        {/* <img class="img-fluid w-100" src={caseSingle.featuredImage.node.sourceUrl} alt={caseSingle.title ? caseSingle.title : 'Image'} /> */}
-        <img class="img-fluid w-100" src={`https://img.youtube.com/vi/${caseSingle.successStoryLink.successStoryLink}/maxresdefault.jpg`} alt={caseSingle.title ? caseSingle.title : 'Image'} />
+        {caseSingle.featuredImage != null ?
+        <img class="img-fluid w-100" src={caseSingle.featuredImage.node.sourceUrl} alt={caseSingle.title ? caseSingle.title : 'Success Story'} />
+        : 
+        <img class="img-fluid w-100" src={`https://img.youtube.com/vi/${caseSingle.successStoryLink.successStoryLink}/maxresdefault.jpg`} alt={caseSingle.title ? caseSingle.title : 'Success Story'} />
+        }
       </button>
     </React.Fragment>
   )
